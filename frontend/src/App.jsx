5a72b8fef073cc0b36fafc6a016c3f3c73f57b0e
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-17 23:44:12 +0530

    Refactor App component to implement routing with HomePage and AdminPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,18 @@
 import "./App.css";
 import ProductCard from "./components/ProductCard";
+import { Route, Routes } from "react-router-dom";
+import AdminPage from "./pages/adminPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <>
-      <div className="h-[700px] w-[700px]  border-[5px] relative flex justify-center items-center">
-        <div className="w-[300px] h-[100px] relative bg-blue-400 flex justify-center items-center">
-          <button className="bg-red-900 absolute top-[0px] right-[0px] ">
-            x
-          </button>
-          <button className="text-white bg-green-500 fixed bottom-[0px] right-[0px] p-[20px]">
-            chat with whatsapp
-          </button>
-          your time has over
-        </div>
-        <div className="w-[300px] h-[300px] bg-pink-500 p-[20px] m-[20px]">
-          <div className="w-[50px] h-[50px] bg-yellow-500"></div>
-
-          <div className="w-[50px] h-[50px] bg-blue-500"></div>
-        </div>
-      </div>
-    </>
+    <div className="w-full h-[100vh] bg-red-500">
+      <Routes path="/">
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<h1>register page</h1>} />
+        <Route path="/admin/*" element={<AdminPage />} />
+      </Routes>
+    </div>
   );
 }
 
