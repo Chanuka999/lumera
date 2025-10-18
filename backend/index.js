fb13d84df96c2f50ce9dbeb8d9e358dfb38ae777
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-19 00:39:25 +0530

    Add CORS middleware and update API routes to include prefix

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import studentRouter from "./routes/studentRouter.js";
 import userRouter from "./routes/userRouter.js";
@@ -9,6 +10,7 @@ import productRouter from "./routes/productRouter.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 dotenv.config();
 app.use((req, res, next) => {
   const authHeader = req.header("Authorization");
@@ -19,7 +21,7 @@ app.use((req, res, next) => {
   // Safely remove Bearer prefix if present
   const token = authHeader.replace(/^Bearer\s+/i, "");
 
-  jwt.verify(token, "jwt-secret", (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err || !decoded) {
       return res
         .status(401)
@@ -41,9 +43,9 @@ mongoose
     console.log("not conected");
   });
 
-app.use("/students", studentRouter);
-app.use("/users", userRouter);
-app.use("/product", productRouter);
+app.use("/api/students", studentRouter);
+app.use("/api/users", userRouter);
+app.use("/api/product", productRouter);
 
 app.listen(5000, () => {
   console.log("server is running on port 5000");
