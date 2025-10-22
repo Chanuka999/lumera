30259d50003cced24af18410292a4eaa76687a4a
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-22 15:34:00 +0530

    Add Loader component and update ProductCard and HomePage to use new structure

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,12 +1,30 @@
 import "./ProductCard.css";
 
-const ProductCard = ({ name, price, images }) => {
+const ProductCard = (props) => {
+  const product = props.product;
   return (
-    <div className="productCard">
-      <h1>{name}</h1>
-      <p>{price}</p>
-      <img className="productImage" src={images} alt="" />
-      <button>add to cart</button>
+    <div className="w-[300px] h-[400px] shadow-2xl flex flex-col m-3 p-[10px]">
+      <img
+        className="w-full h-[250px] object-cover"
+        src={product.images[0]}
+        alt=""
+      />
+      <h1 className="text-xl font-bold text-secondary">{product.name}</h1>
+      {product.labelPrice > product.price && (
+        <div className="flex gap-3 items-center">
+          <p className="text-lg text-secondary font-semibold line">
+            LKR{product.labelPrice.toFixed(2)}
+          </p>
+          <p className="text-lg text-accent font-semibold">
+            LKR{product.price.toFixed(2)}
+          </p>
+        </div>
+      )}
+      <p className="text-sm text-secondary/70">{product.productId}</p>
+      <p className="text-sm text-secondary/70">{product.category}</p>
+      <button className="w-full h-[30px] border border-accent text-accent hover:bg-accent hover:text-white">
+        View Product
+      </button>
     </div>
   );
 };
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import { Route, Routes } from "react-router-dom";
+import ProductPage from "./productPage";
 
 const HomePage = () => {
   return (
@@ -8,7 +9,7 @@ const HomePage = () => {
       <Header />
       <Routes>
         <Route path="/" element={<h1>welcome to the homepage</h1>} />
-        <Route path="/products" element={<h1>product list</h1>} />
+        <Route path="/products" element={<ProductPage />} />
         <Route path="/contact" element={<h1>contact us</h1>} />
         <Route path="/about" element={<h1>About us</h1>} />
         <Route path="/*" element={<h1>404 not found</h1>} />
